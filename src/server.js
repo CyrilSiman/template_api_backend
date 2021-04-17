ca72b366a71e7d8a41c0fef27984341173ef57b2
Cyril Siman <csiman.phone@gmail.com>
2021-04-17 22:12:57 +0200

    Update dependencies

--- src/server.js
+++ src/server.js
@@ -40,7 +40,7 @@ const corsOptions = {
     optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
     credentials: true,
     maxAge:7200,
-    'methods': 'GET,POST',
+    'methods': 'GET,POST,PUT,DELETE',
 }
 
 var cookieExtractor = function(req) {
